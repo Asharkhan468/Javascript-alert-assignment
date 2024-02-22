daaa3eb40571fa021307f8a11706426775d10795
Asharkhan468 <asharullah.khan2468@gmail.com>
2024-02-22 17:15:17 +0500

    Javascript assignment ch1-ch-2

---
+++ app.js
@@ -0,0 +1,93 @@
+
+// Chapter1 alerts practicing started
+
+alert("Hello! Programmers & Developers");
+
+alert("Error! please Enter a Valid Password");
+
+alert("Welcome to JS Land... \n Happy Coding");
+
+alert("Welcome to JS Land");
+
+alert("Happy Coding \n Prevent this page from creating additional dialog");
+
+alert("Hello! I can run javascript alert through browser console");
+
+
+
+// Chapter1 alerts practicing ended
+
+
+
+
+// Chapter2 variable for string started
+
+
+var userName="Ali Ahmed";
+
+alert(userName);
+
+var myName="Ashar Khan";
+
+alert(myName);
+
+var message="Hello world";
+
+alert(message);
+
+var Name="Zohaib";
+var FatherName="Jabar";
+var rollNo= 24556;
+var Class="matric";
+
+
+alert(Name );
+alert(FatherName);
+alert(rollNo);
+alert(Class);
+
+
+// var a="Pizza";
+// b="pizz";
+// c="piz";
+// d="pi";
+// e="p";
+
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+var email="asharkhan@gmail.com";
+
+alert("My email is "+ email);
+
+
+var book="A smarter way to learn javascript";
+
+alert("I am trying to learn from the book " + book);
+
+
+// Chapter2 variable for string started
+
+
+
+
+// Chapter3 variable for number practicing
+
+
+var age=17;
+
+alert("I am year " + age +" old");
+
+
+var a=15;
+
+alert("You have visited this site 14 times");
+
+
+
+
+
+
+
+//Chapter3 variable for number practicing
+
+
